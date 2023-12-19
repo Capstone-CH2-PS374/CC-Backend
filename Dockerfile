1a14b99137f1eb5b5fb43d557cef6a72f82adc54
dickyirsyadf <dickyirsyadf@gmail.com>
2023-12-19 19:25:42 +0700

    adding seed and test deploy

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Use the official Node image.
+# https://hub.docker.com/_/node
+FROM node:18
+
+# Copying this separately prevents re-running npm install on every code change.
+COPY package*.json ./
+RUN npm install
+
+# Copy local code to the container image.
+COPY . .
+
+# Configure and document the service HTTP port.
+ENV PORT 3000
+
+# Run the web service on container startup.
+CMD ["npm","run", "start"]
---
+++ prisma/seed.js
@@ -0,0 +1,25 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+  const category = await prisma.Category.createMany({
+    data:[
+        {categoryId:1, category: 'Pet and Animal Service'},
+        {categoryId:2,category: 'Healthcare'},
+        {categoryId:3,category: 'Youth Development'},
+        {categoryId:4,category: 'Enviromental'},
+        {categoryId:5,category: 'Event Organizer'},
+        {categoryId:6,category: 'IT'},
+    ]
+  })
+  console.log({category})
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
